4519a5c662555e324531f8329a0350791c161bf1
Elia Biraschi <elia.biraschi@gmail.com>
2021-08-22 12:47:30 +0200

    changed readAcc script to register time to the nanoseconds

--- cli/readAcc.sh
+++ cli/readAcc.sh
@@ -5,7 +5,7 @@ echo "T,X,Y,Z" >> /tmp/accel_data.csv
 
 while :
 do
-	T=$(date +%s)
+	T=$(date +%s%N)
 	X=$(cat /sys/bus/iio/devices/iio:device2/in_accel_x_raw)
 	Y=$(cat /sys/bus/iio/devices/iio:device2/in_accel_y_raw)
 	Z=$(cat /sys/bus/iio/devices/iio:device2/in_accel_z_raw)
